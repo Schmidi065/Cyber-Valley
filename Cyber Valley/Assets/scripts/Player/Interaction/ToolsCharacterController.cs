02cea4b76850d388eff093d915da87f3056c2344
Schmidi065 <123089935+Schmidi065@users.noreply.github.com>
2024-01-05 13:46:40 +0100

    object destroy

---
+++ Cyber Valley/Assets/scripts/Player/Interaction/ToolsCharacterController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolsCharacterController : MonoBehaviour
+{
+    PlayerController Player;
+    Rigidbody2D rgbd2d;
+    [SerializeField] float offsetDistance = 1f;
+    [SerializeField] float sizeOfInteractableArea = 1.2f;
+
+    private void Awake()
+    {
+        Player = GetComponent<PlayerController>();
+        rgbd2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UseTool();
+        }
+    }
+
+    private void UseTool()
+    {
+        Vector2 position = rgbd2d.position + Player.lastMotionVector * offsetDistance;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, sizeOfInteractableArea);
+
+        foreach (Collider2D c in colliders)
+        {
+            ToolHit hit = c.GetComponent<ToolHit>();
+            if (hit != null)
+            {
+                hit.Hit();
+                break;
+            }
+        }
+    }
+}
